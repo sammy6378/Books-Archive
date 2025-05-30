428b292c513e921750f35fc89213f95f5e52a339
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 12:23:21 +0300

    feat: refactor database module to use getOrThrow for configuration values

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,28 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import 'dotenv/config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { join } from 'path';
-
-// pnpm install --save @nestjs/typeorm typeorm mysql2 @nestjs/config
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: parseInt(configService.get('POSTGRES_PORT') ?? '5432', 10),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-        synchronize: configService.get('DB_SYNC') === 'true',
-        logging: configService.get('DB_LOGGING') === 'true',
-        migrations: [join(process.cwd(), '/../migrations/**/*{.js,ts}')],
-        autoLoadEntities: true,
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: configService.getOrThrow<number>('DB_PORT'),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        database: configService.getOrThrow<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: configService.getOrThrow<boolean>('DB_SYNC', true),
+        logging: configService.getOrThrow<boolean>('DB_LOGGING', false),
+        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
       }),
       inject: [ConfigService],
     }),
