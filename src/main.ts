d2f7144ceaba1a11b35ebc588cc54da26b46e632
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 10:34:36 +0300

    feat: enhance main.ts with global validation, exception handling, and logging configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { AllExceptionsFilter } from './http-exceptions.filter';
+import { ConfigService } from '@nestjs/config'; //npm install @nestjs/config
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // nestjs instance
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'], // Enable all log levels
+  });
+
+  // global validation pipes
+  app.useGlobalPipes(new ValidationPipe());
+
+  // exception filters
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // global prefix for routes
+  app.setGlobalPrefix('api');
+
+  // server config
+  const configService = app.get(ConfigService);
+  const PORT = configService.getOrThrow<number>('PORT') || 3000;
+
+  await app.listen(PORT);
+
+  Logger.log(`Server is running on http://localhost:${PORT}`);
 }
 bootstrap();
