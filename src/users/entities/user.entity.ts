1a66b3c15f22c689dc0dc08c7a69a219a5c82f40
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 11:11:22 +0300

    feat: implement User entity with fields, relationships, and validation for user management

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,47 @@
-export class User {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+import { status } from '../dto/create-user.dto';
+import { Profile } from 'src/profile/entities/profile.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: status, default: true })
+  isActive: status;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  //   relationships [1 - 1 ]
+  @OneToOne(() => Profile, (profile) => profile.user, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    eager: true,
+  })
+  @JoinColumn()
+  profile: Relation<Profile>;
+}
