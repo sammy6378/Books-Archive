a7c771b8005ee107b9fab9658a9f6377586cbdc4
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 13:10:53 +0300

    refactor: change import statement for bcrypt to use namespace import

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,5 +1,5 @@
 // hash password function
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 export const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 10; // You can adjust the salt rounds for security vs performance trade-off
