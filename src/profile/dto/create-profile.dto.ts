c23e27b4b0624a2664bc1594dc128831a9cd3501
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 11:10:58 +0300

    feat: implement CreateProfileDto with validation decorators for profile creation

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProfileDto {}
+import { IsDateString, IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class CreateProfileDto {
+  @IsNumber()
+  id: number;
+
+  @IsString()
+  bio: string;
+
+  @IsUrl()
+  avatar: string;
+
+  @IsDateString()
+  dateOfBirth: string;
+
+  @IsString()
+  location: string;
+}
