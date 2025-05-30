8a2ce4d2a9f30b278c98a86010f05964d6f9c4a0
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 10:34:28 +0300

    feat: enhance AppModule with middleware and module imports for improved functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ProfileModule } from './profile/profile.module';
+import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
+import { CategoryModule } from './category/category.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { DatabaseModule } from './database/database.module';
+import { LogsModule } from './logs/logs.module';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    ProfileModule,
+    AuthorsModule,
+    BooksModule,
+    CategoryModule,
+    ReviewsModule,
+    DatabaseModule,
+    LogsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        '/users',
+        '/authors',
+        '/books',
+        '/reviews',
+        '/category',
+        '/profile',
+      );
+  }
+}
