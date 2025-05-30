28802b01ee53036f8b89e411854988b06cfa882a
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 11:11:13 +0300

    feat: define CreateUserDto with validation rules and status enum for user creation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,41 @@
-export class CreateUserDto {}
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export enum status {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
+export class CreateUserDto {
+  @IsNumber()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string;
+
+  @IsEnum(status)
+  isActive: status;
+
+  @IsDateString()
+  createdAt: Date;
+
+  @IsDateString()
+  updatedAt: Date;
+}
