f2e409bcc2faa63ccfdf89b24e54149b6865aad1
sammy6378 <samuel202mwangi@gmail.com>
2025-05-30 11:11:05 +0300

    feat: implement Profile entity with fields and relationships for user profiles

--- src/profile/entities/profile.entity.ts
+++ src/profile/entities/profile.entity.ts
@@ -1,1 +1,40 @@
-export class Profile {}
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+
+@Entity('profiles')
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  bio?: string;
+
+  @Column({ nullable: true })
+  avatar?: string;
+
+  @Column({ type: 'date', nullable: true })
+  dateOfBirth?: string;
+
+  @Column({ nullable: true })
+  location?: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  //   relationships
+  @OneToOne(() => User, (user) => user.profile)
+  user: Relation<User>;
+}
