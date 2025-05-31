ae509acaabcc9710a0a28dc290e1e01f7ee55163
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:47:19 +0300

    feat: add TypeORM integration for Book and Author entities in BooksModule

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Author } from 'src/authors/entities/author.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Book, Author])],
   controllers: [BooksController],
   providers: [BooksService],
 })
