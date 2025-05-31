1743b1efbd74b89d7eadda589955cbdb5440fed3
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:06:27 +0300

    fix: update userId type to number in CreateReviewDto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,15 @@
-export class CreateReviewDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateReviewDto {
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  rating: number;
+
+  @IsString()
+  userId: string;
+  // bookId: string; // Uncomment if you want to include bookId in the DTO
+}
