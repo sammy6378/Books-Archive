565072870ba0d1768fe2c98450dd609ccf14a0cc
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:06:38 +0300

    feat: implement Review entity with properties and relationships

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,41 @@
-export class Review {}
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column()
+  rating: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.reviews, {
+    onDelete: 'CASCADE',
+  })
+  user: Relation<User>;
+
+  // @ManyToOne(() => Book, (book) => book.reviews, {
+  //   eager: true,
+  //   onDelete: 'CASCADE',
+  // })
+  // book: Relation<Book>;
+}
