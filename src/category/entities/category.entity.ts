082a2e025013e440b0474f690cd713e29e5e096f
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 22:16:55 +0300

    feat: implement Category entity with properties and ManyToMany relationship to Book

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,32 @@
-export class Category {}
+import { Book } from 'src/books/entities/book.entity';
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @ManyToMany(() => Book, (book) => book.categories)
+  books: Relation<Book[]>;
+}
