76822d4445ab271016de9d7001ac709d4c15e69e
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:06:16 +0300

    fix: add imports for DatabaseModule and TypeOrmModule in ReviewsModule

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Review, User])],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
