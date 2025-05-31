21e6475692ddd00144a9be4685a653d783ae38be
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:47:49 +0300

    feat: define CreateBookDto with validation rules for book creation

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,20 @@
-export class CreateBookDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  publicationYear: number;
+
+  @IsBoolean()
+  isAvailable: boolean;
+
+  @IsString()
+  authorId: string;
+}
