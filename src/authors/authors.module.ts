6894e686cbe03cc61202f1230e7b54c179b72ea6
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:19:39 +0300

    feat: add TypeOrmModule and DatabaseModule imports to AuthorsModule

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Author } from './entities/author.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Author])],
   controllers: [AuthorsController],
   providers: [AuthorsService],
 })
