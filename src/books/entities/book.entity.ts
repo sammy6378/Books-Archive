4a1c41b653669f7d51acdcca5eded38e3d6e718c
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:47:59 +0300

    feat: implement Book entity with properties and relationship to Author

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,42 @@
-export class Book {}
+import { Author } from 'src/authors/entities/author.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+
+@Entity('books')
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  publicationYear: number;
+
+  @Column({ type: 'boolean', default: true })
+  isAvailable: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @ManyToOne(() => Author, (author) => author.book, {
+    onDelete: 'CASCADE',
+    eager: true,
+  })
+  author: Relation<Author>;
+}
