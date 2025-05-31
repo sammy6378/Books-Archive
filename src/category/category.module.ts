9d4aeec12edf4bff6ed3ca539135b56be215ae4f
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 22:18:30 +0300

    feat: add TypeOrmModule imports for Category and Book entities in CategoryModule

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Book } from 'src/books/entities/book.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Category, Book])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
