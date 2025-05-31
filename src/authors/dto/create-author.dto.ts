1853f28ee0a04a94fbe806c10837b02eac097280
sammy6378 <samuel202mwangi@gmail.com>
2025-05-31 21:20:14 +0300

    fix: update CreateAuthorDto to include validation for optional fields

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAuthorDto {}
+import { IsBoolean, IsDateString, IsOptional, IsString } from 'class-validator';
+
+export class CreateAuthorDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  bio: string;
+
+  @IsDateString()
+  @IsOptional()
+  birthDate: Date;
+
+  @IsBoolean()
+  isActive: boolean;
+}
