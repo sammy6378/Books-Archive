d1153fde26bcffb2452e0f34b37b8fb68c6ab6f5
sammy6378 <samuel202mwangi@gmail.com>
2025-06-05 06:18:26 +0300

    feat: implement global caching with CacheModule and Keyv integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,36 @@ import { CategoryModule } from './category/category.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { DatabaseModule } from './database/database.module';
 import { LogsModule } from './logs/logs.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerMiddleware } from './logger.middleware';
 import { SeedModule } from './seed/seed.module';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { CacheableMemory } from 'cacheable';
+import { createKeyv, Keyv } from '@keyv/redis';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+
+    // global cache
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (configService: ConfigService) => {
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 30000, lruSize: 5000 }),
+            }),
+            createKeyv(configService.getOrThrow<string>('REDIS_URL')),
+          ],
+          Logger: true,
+        };
+      },
     }),
     UsersModule,
     ProfileModule,
@@ -28,7 +49,12 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
